024087fb91b755482fa765a66c1ba9145b27b85a
Oropeza Vilchis Luis Alberto <luis.oropeza.129@gmail.com>
2017-09-03 17:53:06 -0500

    Clase Punto y Linea agregadas, Liang-Barsky implementado (codigo repetido, ¡refactorizar!) y algunas modificaciones en mas.

--- liang_barsky_algorithm/line_clipping.py
+++ liang_barsky_algorithm/line_clipping.py
@@ -8,49 +8,181 @@
 # Hecho en Python 3.5
 
 
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def to_string(self):
+        return "({0}, {1})".format(self.x, self.y)
+
+
 class Window:
-    def __init__(self, point=list(), width=0, height=0):
+    def __init__(self, point, width=0, height=0):
         self.point = point
         self.width = width
         self.height = height
 
+    def get_x_left(self):
+        return self.point.get_x()
+
+    def get_x_right(self):
+        return self.point.get_x() + self.width
+
+    def get_y_bottom(self):
+        return self.point.get_y()
+
+    def get_y_top(self):
+        return self.point.get_y() + self.height
+
+    def is_inside_window(self, point):
+        if point.get_x() < self.get_x_left() or point.get_x() > self.get_x_right():
+            return False
+        elif point.get_y() < self.get_y_bottom() or point.get_y() > self.get_y_top():
+            return False
+        else:
+            return True
+
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def get_x_i(self):
+        return self.point_1.get_x()
+
+    def get_x_f(self):
+        return self.point_2.get_x()
+
+    def get_y_i(self):
+        return self.point_1.get_y()
+
+    def get_y_f(self):
+        return self.point_2.get_y()
+
+    def is_vertical(self):
+        return True if (self.get_x_f() - self.get_x_i()) == 0 else False
+
+    def is_horizontal(self):
+        return True if (self.get_y_f() - self.get_y_i()) == 0 else False
+
+    def is_parallel_to_axis(self):
+        return True if self.is_vertical() or self.is_horizontal() else False
+
 
 def get_data():
     points = list()
-    tmp = list()
-    file = open('input.txt', 'r')
-    content = file.readline()
-    while content != "":
-        content = content.rstrip('\n')
-        for number in content.split(" "):
-            tmp.append(int(number))
-        points.append(tmp)
-        tmp = []
-        content = file.readline()
-    file.close()
-
+    with open('input.txt', 'r') as file:
+        for line in file:
+            data = list(map(int, line.rstrip("\n").split(" ")))
+            points.append(Point(data[0], data[1]))
     return points
 
 
-def left_analysis(point_1, point_2):
-    print(point_1, point_2)
+def get_lines(points):
+    lines = []
+    for (p1, p2) in zip(points[::2], points[1::2]):
+        lines.append(Line(p1, p2))
+    return lines
 
 
-def line_clipper(points, window):
-    i = 0
-    while i < len(points) - 1:
-        left_analysis(points[i], points[i+1])
-        i += 1
+def left_analysis(line, w):
+    if line.is_parallel_to_axis():
+        return None
+    u = (w.get_x_left() - line.get_x_i())/(line.get_x_f() - line.get_x_i())
+    if u < 0 or u > 1:
+        return None
+    y = line.get_y_i() + u*(line.get_y_f() - line.get_y_i())
+    clipping_point = Point(w.get_x_left(), y)
+    if not w.is_inside_window(clipping_point):
+        return None
+    return clipping_point
+
+
+def right_analysis(line, w):
+    if line.is_parallel_to_axis():
+        return None
+    u = (w.get_x_right() - line.get_x_i())/(line.get_x_f() - line.get_x_i())
+    if u < 0 or u > 1:
+        return None
+    y = line.get_y_i() + u*(line.get_y_f() - line.get_y_i())
+    clipping_point = Point(w.get_x_left(), y)
+    if not w.is_inside_window(clipping_point):
+        return None
+    return clipping_point
+
+
+def bottom_analysis(line, w):
+    if line.is_parallel_to_axis():
+        return None
+    u = (w.get_y_bottom() - line.get_y_i())/(line.get_y_f() - line.get_y_i())
+    if u < 0 or u > 1:
+        return None
+    x = line.get_x_i() + u*(line.get_x_f() - line.get_x_i())
+    clipping_point = Point(x, w.get_y_bottom())
+    if not w.is_inside_window(clipping_point):
+        return None
+    return clipping_point
+
+
+def top_analysis(line, w):
+    if line.is_parallel_to_axis():
+        return None
+    u = (w.get_y_top() - line.get_y_i())/(line.get_y_f() - line.get_y_i())
+    if u < 0 or u > 1:
+        return None
+    x = line.get_x_i() + u*(line.get_x_f() - line.get_x_i())
+    clipping_point = Point(x, w.get_y_top())
+    if not w.is_inside_window(clipping_point):
+        return None
+    return clipping_point
+
+
+def liang_barsky(line, window):
+    intersections = dict()
+    intersections["left"] = left_analysis(line, window)
+    intersections["right"] = right_analysis(line, window)
+    intersections["top"] = top_analysis(line, window)
+    intersections["bottom"] = bottom_analysis(line, window)
+    return intersections
+
+
+def line_clipper(lines, window):
+    line_counter = 1
+    for l in lines:
+        clipping_points = liang_barsky(l, window)
+        store_information(clipping_points["left"], "Left", line_counter)
+        store_information(clipping_points["right"], "Right", line_counter)
+        store_information(clipping_points["top"], "Top", line_counter)
+        store_information(clipping_points["bottom"], "Bottom", line_counter)
+        line_counter += 1
+
+
+def store_information(clipping_point, side, line_id):
+    with open('output.txt', 'a') as file:
+        message = "{0} limit, line {1}: {2}\n"
+        if clipping_point:
+            message = message.format(side, line_id, clipping_point.to_string())
+        else:
+            message = message.format(side, line_id, "it has no clipping point.")
+        file.write(message)
 
 
 def main():
-    points = get_data()
+    lines = get_lines(get_data())
     print("===Enter Window data===")
-    w_point = input("Point (separate by space): ").split(" ")
+    w_point = list(map(int, input("Point (separate by space): ").split(" ")))
     width = int(input("Width: "))
     height = int(input("Height: "))
-    w = Window(w_point, width, height)
-    line_clipper(points, w)
+    w = Window(Point(w_point[0], w_point[1]), width, height)
+    line_clipper(lines, w)
 
 
 if __name__ == '__main__':
