0ab25120c06cd804a5f37551cf95eba4970a3777
Luis Alberto Oropeza Vilchis <luis.oropeza.129@gmail.com>
2017-09-09 20:50:43 -0500

    Mensaje de licencia agregado.

--- liang_barsky_algorithm/line_clipping.py
+++ liang_barsky_algorithm/line_clipping.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Universidad Nacional Autonoma de Mexico / Facultad de Ingenieria
-# Computación Gráfica
-# Alumnos:
-# Barriga Martínez Diego Alberto
-# Vilchis Oropeza Luis Alberto
-# Hecho en Python 3.5
+"""
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Realizado por:
+	@Barriga Martínez Diego Alberto
+	@Vilchis Oropeza Luis Alberto
+
+	Versión de Python:s 3.6
+"""
 
 
 class Point:
